d628f9c9579629a60dd0c9831d4e48b9146277f4
Irani Romero <iranimromero@gmail.com>
2021-11-09 20:05:35 -0300

    Se imprimer el usuario

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -1,10 +1,13 @@
-
+//Irani Romero <iranimromero@gmail.com>
 public class EjemploGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Git");
+		
+		String usuario ="Irani";
 	
+		System.out.println(usuario);
 	}
 
 }
